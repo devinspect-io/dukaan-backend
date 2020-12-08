e7b01befe17eb1b0c11b79d8c1b9d5e4803c5265
mustafakhalidcs <mustafakhalidcs@gmail.com>
2020-12-09 01:47:28 +0500

    returns payload in title case

--- utils.py
+++ utils.py
@@ -2,6 +2,13 @@ from bson.objectid import ObjectId
 from copy import deepcopy
 
 
+def change_case(obj, case=None):
+    for k in obj.keys():
+        if isinstance(obj[k], str):
+            obj[k] = obj[k].lower() if case == "lower" else obj[k].title()
+    return obj
+
+
 def clean_dict_helper(d):
     if isinstance(d, ObjectId):
         return str(d)
@@ -11,13 +18,7 @@ def clean_dict_helper(d):
 
     if isinstance(d, dict):
         for k, v in deepcopy(d).items():
+            if isinstance(v, str):
+                v = v.title()
             d.update({k: clean_dict_helper(v)})
-
     return d
-
-
-def change_case(obj, case=None):
-    for k in obj.keys():
-        if isinstance(obj[k], str):
-            obj[k] = obj[k].lower() if case == "lower" else obj[k].title()
-    return obj
